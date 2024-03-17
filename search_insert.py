00ff0706c1128eec00e68434143d885038d59d8c
Supratim Das <supratim.gre2014@yahoo.com>
2024-03-17 11:41:07 +0100

    Create search_insert.py

---
+++ search_insert.py
@@ -0,0 +1,28 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low)//2
+
+            if nums[mid] == target:
+                return mid
+            elif (nums[mid] < target):
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return low
+
+# Example 1:
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+
+# Example 2:
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+
+# Example 3:
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
